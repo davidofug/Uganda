c30594e84a6cc9f68c57c105bc57c4769805fd7e
David Wampamba <davidwampamba@gmail.com>
2019-04-02 17:51:26 +0300

    Create district.institutes.js

---
+++ institutes/district.institutes.js
@@ -0,0 +1,116 @@
+'''
+Institutes and Universities in the Districts of Uganda
+Python 2.7+
+'''
+
+institutes = {
+	'Kampala': [
+		'Makerere University', 
+		'Kyambogo University', 
+		'Makerere University Business School', 
+		'Uganda Management Institute', 
+		'Africa Graduate University', 
+		'International Health Sciences University', 
+		'St. Lawrence University', 
+		'International University of East Africa', 
+		'Kampala International University', 
+		'Aga Khan University', 
+		'Cavendish University Uganda', 
+		'St. Augustine International University', 
+		'Uganda Technology and Management University', 
+		'Victoria University Uganda', 
+		'Virtual University of Uganda', 
+		'Kampala University', 
+		'Africa Renewal University', 
+		'India Africa Institute of Trade', 
+		'International School of Business and Technology', 
+		'Institute of Petroleum Studies Kampala', 
+		'Royal Open University'
+	], 
+	'Busia': [
+		'Busitema University'
+	],
+	'Gulu': [
+		'Gulu University'
+	], 
+	'Kabale': [
+		'Kabale University'
+	], 
+	'Lira': [
+		'Lira University', 
+		'All Saints University'
+	], 
+	'Mbarara': [
+		'Mbarara University of Science and Technology', 
+		'Bishop Stuart University'
+	], 
+	'Arua': [
+		'Muni University'
+	], 
+	'Soroti': [
+		'Soroti University'
+	], 
+	'Mpigi': [
+		'Uganda Martyrs University'
+	], 
+	'Luweero': [
+		'Ndejje University', 
+		'Bugema University'
+	], 
+	'Mukono': [
+		'Uganda Christian University'
+	], 
+	'Mbale': [
+		'Islamic University in Uganda', 
+		'LivingStone International University'
+	], 
+	'Masaka': [
+		'Muteesa I Royal University'
+	], 
+	'Kumi': [
+		'Kumi University'
+	], 
+	'Kanungu': [
+		'Great Lakes Regional University'
+	], 
+	'Wakiso': [
+		'Nkumba University', 
+		'African Bible University', 
+		'University of Kisubi'
+	], 
+	'Kagadi': [
+		'African Rural University'
+	], 
+	'Sheema': [
+		'Ankole Western University'
+	], 
+	'Jinja': [
+		'Nsaka University', 
+		'Uganda Senior Command and Staff College', 
+		'Uganda Junior Staff College'
+	], 
+	'Iganga': [
+		'Busoga University'
+	], 
+	'Ibanda': [
+		'Ibanda University'
+	], 
+	'Fort Portal': [
+		'Mountains of the Moon University', 
+		'Uganda Pentecostal University'
+	], 
+	'Buikwe': [
+		'University of Military Science and Technology'
+	], 
+	'Mubende': [
+		'Uganda Military Academy'
+	], 
+	'Kiryandongo': [
+		'Kigumba Petroleum Institute'
+	]
+}
+
+//Example
+
+print institutes['Kiryandongo'][0];
+
